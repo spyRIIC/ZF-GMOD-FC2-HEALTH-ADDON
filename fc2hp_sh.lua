f6cb7e7fb6cc1495686291a2026980bf763663b8
spyRIIC <67972930+spyRIIC@users.noreply.github.com>
2020-07-07 20:42:03 +0200

    Add files via upload

---
+++ fc2hp_sh.lua
@@ -0,0 +1,59 @@
+AddCSLuaFile("client/fc2hp_cl.lua")
+AddCSLuaFile("fc2hp_sh.lua")
+
+-- Syrette v_model
+resource.AddFile("models/zoey/syrette5.mdl")
+resource.AddFile("models/zoey/syrette5.dx80.vtx")
+resource.AddFile("models/zoey/syrette5.dx90.vtx")
+resource.AddFile("models/zoey/syrette5.sw.vtx")
+resource.AddFile("models/zoey/syrette5.vvd")
+resource.AddFile("models/zoey/syrette5.xbox.vtx")
+
+-- Materials for the l4d2 adrenaline shot used in the v_model
+resource.AddFile("materials/models/bloocobalt/l4d/items/adrenaline_shot.vmt")
+resource.AddFile("materials/models/bloocobalt/l4d/items/adrenaline_shot.vtf")
+resource.AddFile("materials/models/bloocobalt/l4d/items/adrenaline_shot_clear.vmt")
+resource.AddFile("materials/models/bloocobalt/l4d/items/adrenaline_shot_clear.vtf")
+resource.AddFile("materials/models/bloocobalt/l4d/items/adrenaline_shot_plastic.vmt")
+resource.AddFile("materials/models/bloocobalt/l4d/items/adrenaline_shot_plastic.vtf")
+
+-- Materials for the l4d medpack used for increasing max syrettes carried
+resource.AddFile("materials/models/bloocobalt/l4d/items/w_eq_medkit.vmt")
+resource.AddFile("materials/models/bloocobalt/l4d/items/w_eq_medkit.vtf")
+
+-- Materials for the first aid locker
+resource.AddFile("materials/models/generic/firstaid_diffuse.vmt")
+resource.AddFile("materials/models/generic/firstaid_diffuse.vtf")
+
+-- Model for the first aid locker
+resource.AddFile("models/generic/firstaid_ievel.dx80.vtx")
+resource.AddFile("models/generic/firstaid_ievel.dx90.vtx")
+resource.AddFile("models/generic/firstaid_ievel.mdl")
+resource.AddFile("models/generic/firstaid_ievel.phy")
+resource.AddFile("models/generic/firstaid_ievel.sw.vtx")
+resource.AddFile("models/generic/firstaid_ievel.vvd")
+
+-- Model for the w_model for the l4d2 adrenaline w_model
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_adrenaline.mdl")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_adrenaline.dx90.vtx")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_adrenaline.dx80.vtx")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_adrenaline.phy")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_adrenaline.sw_vtx")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_adrenaline.vvd")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_adrenaline.xbox.vtx")
+
+-- Model for the medpack used for increasing max syrettes carried
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_fieldkit.mdl")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_fieldkit.dx90.vtx")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_fieldkit.dx80.vtx")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_fieldkit.phy")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_fieldkit.sw_vtx")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_fieldkit.vvd")
+resource.AddFile("models/bloocobalt/l4d/items/w_eq_fieldkit.xbox.vtx")
+
+-- Materials for the syrette hud, (Thanks johnny, u ish the greatest =3)
+resource.AddFile("materials/HUD/syrmat.vtf")
+resource.AddFile("materials/HUD/syrmatz.vmt")
+
+-- Sound for using the syrette
+resource.AddFile("sound/syr/syr_usefinal.wav")
